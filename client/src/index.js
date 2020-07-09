9178e02396a14ae1f5096101b8fc92ef67f5acc5
bomi94436 <boo072963@naver.com>
2020-07-09 10:46:59 +0900

    setting: node server

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import "./index.css";
-import App from "components/App";
+import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
-import store from "modules/store";
+import store from "./modules/store";
 
 ReactDOM.render(
   <Provider store={store}>
